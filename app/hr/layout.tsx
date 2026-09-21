d9591c40863cf816b061c9ccfaa7a241b7b330c0
hrnpw <info.nernplubwan@gmail.com>
2026-09-21 17:03:46 +0700

    Delete app directory

--- app/hr/layout.tsx
+++
@@ -1,5 +0,0 @@
-import HrLayout from './HrLayout';
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <HrLayout>{children}</HrLayout>;
-}
--- app/teacher/layout.tsx
+++
@@ -1,5 +0,0 @@
-import TeacherLayout from './TeacherLayout';
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <TeacherLayout>{children}</TeacherLayout>;
-}
